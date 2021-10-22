cdfda6193c33b0290a6403aa15584e0f28f46084
sungtaekoh <ohbass95@gmail.com>
2021-10-22 23:53:05 +0900

    Jsp_project01

---
+++ src/main/java/com/care/root/member/dao/MemberDAO.java
@@ -0,0 +1,11 @@
+package com.care.root.member.dao;
+
+public class MemberDAO {
+	create table jsp_member(
+			id varchar2(20) primary key,
+			pwd varchar2(20),
+			name varchar2(20),
+			addr varchar2(20)
+			);
+
+}
